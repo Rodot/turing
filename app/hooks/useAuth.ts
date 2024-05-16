d2e447eb75e2f447a7e5b73fa55841e750d57506
Aurelien Rodot <4189146+Rodot@users.noreply.github.com>
2024-05-16 22:10:57 +0200

    Fix auth creating two accounts because of react strict mode

--- app/hooks/useAuth.ts
+++ app/hooks/useAuth.ts
@@ -2,24 +2,30 @@ import { supabase } from "@/utils/supabase/client";
 import { User } from "@supabase/supabase-js";
 import { useEffect, useState } from "react";
 
+var loading = false;
+
 export const useAuth = () => {
   const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
-    const checkSession = async () => {
+    const signInOrUp = async () => {
+      if (loading) return;
+      loading = true;
       const {
         data: { session },
       } = await supabase.auth.getSession();
       if (session) {
         setUser(session.user);
+        console.log("Signed in " + session.user.id);
       } else {
         const { data, error } = await supabase.auth.signInAnonymously();
         setUser(data?.user ?? null);
+        console.log("Signed up " + data?.user?.id);
       }
+      loading = false;
     };
 
-    checkSession();
-  }, []);
-
+    signInOrUp();
+  });
   return user;
 };
