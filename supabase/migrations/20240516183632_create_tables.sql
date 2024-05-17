79602cbd53f30fe6a4f4459b3df33b71a7a1e1da
Aurelien Rodot <4189146+Rodot@users.noreply.github.com>
2024-05-17 12:24:17 +0200

    Add migration

---
+++ supabase/migrations/20240516183632_create_tables.sql
@@ -0,0 +1,61 @@
+-- games
+CREATE TABLE public.games(
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    created_at timestamp with time zone DEFAULT now(),
+    updated_at timestamp with time zone DEFAULT now()
+);
+
+-- rooms
+CREATE TABLE public.rooms(
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    current_game_id uuid NOT NULL REFERENCES public.games ON DELETE CASCADE
+);
+
+-- user profiles
+CREATE TABLE public.profiles(
+    id uuid NOT NULL REFERENCES auth.users ON DELETE CASCADE,
+    created_at timestamp with time zone DEFAULT now(),
+    updated_at timestamp with time zone DEFAULT now(),
+    current_room_id uuid REFERENCES public.rooms ON DELETE SET NULL,
+    name text,
+    PRIMARY KEY (id)
+);
+
+-- players
+CREATE TABLE public.players(
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    profile_id uuid NOT NULL REFERENCES public.profiles ON DELETE CASCADE,
+    room_id uuid NOT NULL REFERENCES public.rooms ON DELETE CASCADE
+);
+
+-- messages
+CREATE TABLE public.messages(
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    created_at timestamp with time zone DEFAULT now(),
+    -- game_id uuid NOT NULL REFERENCES public.games ON DELETE CASCADE,
+    author text,
+    content text
+);
+
+ALTER publication supabase_realtime
+    ADD TABLE public.messages;
+
+-- create a public profile when a user is created, using the same id
+CREATE FUNCTION public.handle_new_user()
+    RETURNS TRIGGER
+    LANGUAGE plpgsql
+    SECURITY DEFINER
+    SET search_path = ''
+    AS $$
+BEGIN
+    INSERT INTO public.profiles(id)
+        VALUES(NEW.id);
+    RETURN new;
+END;
+$$;
+
+CREATE TRIGGER on_auth_user_created
+    AFTER INSERT ON auth.users
+    FOR EACH ROW
+    EXECUTE PROCEDURE public.handle_new_user();
+
